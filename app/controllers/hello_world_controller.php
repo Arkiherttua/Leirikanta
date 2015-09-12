a97f764adba235388304cade2a6147a7a22503e9
Arkiherttua <ainorosah@gmail.com>
2015-09-12 20:26:39 +0300

    luotu ekat html-sivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,44 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  //echo('Etusivu!');
+          View::make('home.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
+    
+        public static function hakemus(){
+      // Testaa koodiasi täällä
+      View::make('hakemus.html');
+    }
+    
+        public static function leiri(){
+      // Testaa koodiasi täällä
+      View::make('leiri.html');
+    }
+    
+        public static function leirilista(){
+      // Testaa koodiasi täällä
+      View::make('leirilista.html');
+    }
+    
+        public static function leiripaikka(){
+      // Testaa koodiasi täällä
+      View::make('leiripaikka.html');
+    }
+    
+        public static function muokkaa_hakemusta(){
+      // Testaa koodiasi täällä
+      View::make('muokkaa_hakemusta.html');
+    }
+    
+        public static function profiilisivu(){
+      // Testaa koodiasi täällä
+      View::make('profiilisivu.html');
+    }
+   
+    
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,27 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+   $routes->get('/leiri', function() {
+    HelloWorldController::leiri();
+  });
+  
+    $routes->get('/hakemus', function() {
+    HelloWorldController::hakemus();
+  });
+  
+    $routes->get('/leirilista', function() {
+    HelloWorldController::leirilista();
+  });
+  
+    $routes->get('/leiripaikka', function() {
+    HelloWorldController::leiripaikka();
+  });
+  
+    $routes->get('/muokkaa_hakemusta', function() {
+    HelloWorldController::muokkaa_hakemusta();
+  });
+  
+    $routes->get('/profiilisivu', function() {
+    HelloWorldController::profiilisivu();
+  });
